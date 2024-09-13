df2ee12aa2a7187ad11c7122701fb9631580475e
Devi Prasad Dash <141831694+Devi-Prasad-Dash@users.noreply.github.com>
2024-09-13 11:31:35 +0000

    commit02

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 3012;
+let accountEmail = "deviprasad2003@gmail.com";
+var accountPassword = "1234";
+accountCity = "Bhubaneswar";
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
