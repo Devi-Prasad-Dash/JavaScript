016fe7d695983bdabae6a17dda4d3e9dd11370c6
Devi Prasad Dash <141831694+Devi-Prasad-Dash@users.noreply.github.com>
2024-09-14 15:30:20 +0000

    datatypes

---
+++ 01_Basics/variable.js
@@ -0,0 +1,16 @@
+/*
+let a = null
+let b;
+let c = "Devi"
+let d = 23
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+*/
+
+let e = "Devi";
+let f = Number(e)
+console.log(typeof(f))
+console.log(f)
