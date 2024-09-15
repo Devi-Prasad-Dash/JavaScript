33b8d083c1be247d7457f10bc9fde767c59b9add
Devi Prasad Dash <141831694+Devi-Prasad-Dash@users.noreply.github.com>
2024-09-15 12:07:01 +0000

    Math & Num

---
+++ 01_Basics/math_num.js
@@ -0,0 +1,18 @@
+/*
+let min = 20
+let max = 40
+
+let num = Math.floor(Math.random() * (max-min+1)) + min
+console.log(num)
+
+
+let num = 10000
+console.log(num.toFixed(2))
+console.log(num.toLocaleString('en-IN'))
+console.log(num.toPrecision(3))
+*/
+
+console.log(Math.min(2,3,4,5))
+console.log(Math.min(2,3,4,5))
+console.log(Math.abs(-5))
+console.log(Math.round(5.6))
