3ae336193a63346e24b39f42362157b7808caa7c
Devi Prasad Dash <141831694+Devi-Prasad-Dash@users.noreply.github.com>
2024-09-15 12:34:19 +0000

    dates commit

---
+++ 01_Basics/dates.js
@@ -0,0 +1,19 @@
+let myDate = new Date(2024, 8, 15, 5, 53)
+let myDate2 = new Date("2024-9-15")
+
+/*
+console.log(myDate.toDateString())
+console.log(myDate.toString)
+console.log(myDate.toLocaleString)
+console.log(typeof myDate)
+
+console.log(myDate.toLocaleTimeString())
+console.log(myDate2.toDateString())
+*/
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp)
+console.log(myDate.getTime)
+console.log(myDate.getMonth)
+
+console.log(Math.floor(Date.now()/1000))
