adeb453d49ae7d931d8e27dc3ecc50791ac7c400
Devi Prasad Dash <141831694+Devi-Prasad-Dash@users.noreply.github.com>
2024-09-15 11:13:34 +0000

    string commit

---
+++ 01_Basics/string01.js
@@ -0,0 +1,28 @@
+/*
+
+let userOne = {
+    name : "Devi",
+    age : 21
+}
+
+let userTwo = userOne
+
+userOne.age = 25
+
+console.log(userOne.age)
+console.log(userTwo.age)
+
+const name = "Devi Prasad Dash"
+console.log(name.charAt(3))
+console.log(name.indexOf('D'))
+console.log(name.substring(0,4))
+
+console.log(name.slice(-16, 4))
+console.log(name.replace('D','X'))
+console.log(name.trim())
+console.log(name.split(' '))
+
+*/
+let name = "Devi Prasad"
+let age = 21
+console.log(`Hi ${name} your age is ${age}`)
