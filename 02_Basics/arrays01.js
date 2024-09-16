666f03b7a44e98d398618cb7ec3510aad7928637
Devi Prasad Dash <141831694+Devi-Prasad-Dash@users.noreply.github.com>
2024-09-16 16:50:15 +0000

    Arrays

---
+++ 02_Basics/arrays01.js
@@ -0,0 +1,50 @@
+/*
+
+const myArr = [1,2,3,4,5]
+const myArr2 = new Array(10,20,30)
+
+console.log(myArr)
+console.log(myArr2[2])
+
+myArr.push(6)
+console.log(myArr)
+myArr.pop()
+console.log(myArr)
+
+myArr.unshift(9)
+console.log(myArr)
+myArr.shift()
+console.log(myArr)
+
+console.log(myArr.includes(5))
+console.log(myArr.indexOf(3))
+
+const newArr = myArr.join()
+console.log(newArr)
+console.log(typeof newArr)
+
+
+console.log(myArr.slice(0,2))
+console.log(myArr)
+
+console.log(myArr.splice(1,3))
+console.log(myArr)
+
+arr1.push(arr2)
+
+const arr3 = arr1.concat(arr2)
+console.log(arr3)
+
+const arr4 = [...arr1, ...arr2]
+console.log(arr4)
+
+
+const arr1 = [1, 2, 3, [4, 5, [7, 8]]]
+
+const arr4 = arr1.flat(Infinity)
+console.log(arr4)
+
+*/
+
+const arr1 = [1, 2, 3, 4, 5]
+const arr2 = [6, 7, 8, 9, 10]
