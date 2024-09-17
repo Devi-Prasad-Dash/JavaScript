f86eaec1b664f6b2c305bdfc774c5be2f5de2d95
Devi Prasad Dash <141831694+Devi-Prasad-Dash@users.noreply.github.com>
2024-09-17 04:42:10 +0000

    function02

--- 02_Basics/functions.js
+++ 02_Basics/functions.js
@@ -26,15 +26,37 @@ function loginMessage(username) {
 // console.log(loginMessage("Devi"))
 console.log(loginMessage())
 
-*/
 
 function loginMessage(username = "John") {
-    if (!username) {
-        console.log("Please enter your username")
-        return
-    }
     return `${username} just logged in`
 }
 
 console.log(loginMessage("Devi"))
 console.log(loginMessage())
+
+
+function add(num1, num2, ...num) {
+    return num
+}
+
+console.log(add(1,2,3,4,5,6,7,8))
+
+
+const user = {
+    name : "Devi",
+    age : 21
+}
+
+function handleOnject(object) {
+    return `Your name is ${object.name} and your age is ${object.age}`
+}
+
+console.log(handleOnject(user))
+
+*/
+
+function handleArray(arr1) {
+    return arr1[0]
+}
+
+console.log(handleArray([1,2,3,4]))
